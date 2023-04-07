2186f2ca69af7dbaf06bdb033d8645af28186c68
kamiszo821 <kamilszota2000@gmail.com>
2023-04-07 15:03:53 +0200

    Hot reloading frontend

--- front/gym-app/next.config.js
+++ front/gym-app/next.config.js
@@ -3,4 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
+
+}
