607a6889f4b29ca4ab29b308063091a960e90243
Marcin Knyc <maroxesen@gmail.com>
2023-07-08 16:01:30 +0200

    Fix "Gym is necessary in request payload"

--- GymApp/GymApp/Models/Sector.cs
+++ GymApp/GymApp/Models/Sector.cs
@@ -1,10 +1,12 @@
-﻿namespace GymApp.Models
+﻿using System.Text.Json.Serialization;
+namespace GymApp.Models
 {
     public class Sector : Entity
     {
         public String SectorName { get; set; } = "";
         public Guid GymId { get; set; }
-        public Gym Gym { get; set; }
+        [JsonIgnore]
+        public Gym? Gym { get; set; }
         public List<Entrance> Entrances { get; set; } = new List<Entrance>();
     }
 }
